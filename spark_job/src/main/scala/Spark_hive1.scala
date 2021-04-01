297bb7a1a7614c3dd79e416b413d290fb03a2299
Administrator <Administrator@dell-PC>
2021-04-01 10:34:27 +0800

    spark连接直接读取hive表

--- spark_job/src/main/scala/Spark_hive1.scala
+++ spark_job/src/main/scala/Spark_hive1.scala
@@ -1,5 +1,5 @@
 import org.apache.spark
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 
 object Spark_hive1 {
 
@@ -9,9 +9,18 @@ object Spark_hive1 {
       builder
       .appName("My Spark Application")
       .enableHiveSupport()
+      .config("spark.master","local[*]")
       .getOrCreate
+      spark.sql("use yanke_data")
+      val  df1 =spark.sql("select * from score")
+      df1.show()
 
-      spark.sql("select * from yanke_data.dept").show()
+    //计算信度
+    //mean均值，variance方差，stddev标准差，corr(Pearson相关系数)，skewness偏度，kurtosis峰度
+    val df4=spark.sql("SELECT mean(age),variance(age),stddev(age),corr(age,yearsmarried),skewness(age),kurtosis(age) FROM score")
 
   }
-}
+
+
+
+  }
